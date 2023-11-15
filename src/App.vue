5b7b05ae9d3259f42c06f84cfdedf9e6132b2dfb
LucaLF-dev <laficaraluca@gmail.com>
2023-11-15 11:53:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+  
+  export default {
+    components: {
+
+    },
+    data() {
+       return {
+
+       };
+    },
+  }
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+
+</style>
