44a24491f42a1074d8e635e0dcb3944c0da428dc
LucaLF-dev <laficaraluca@gmail.com>
2023-11-15 12:08:40 +0100

    Fixed: macrostructure app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script >
+import HeaderPage from './components/HeaderPage.vue';
+import HeroComponent from './components/HeroComponent.vue';
+import MainContent from './components/MainContent.vue';
+import FooterPage from './components/FooterPage.vue';
   
   export default {
     components: {
+      HeaderPage,HeroComponent,MainContent,FooterPage,
 
     },
     data() {
@@ -13,6 +18,12 @@
 </script>
 
 <template>
+  <HeaderPage/>
+  <HeroComponent/>
+  <MainContent/>
+  <FooterPage/>
+
+
   
 </template>
 
