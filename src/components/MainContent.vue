3e031e818e2d708d77310cea45824e0020c639a3
LucaLF-dev <laficaraluca@gmail.com>
2023-11-15 12:07:59 +0100

    ADD - macrostructure to app.vue

---
+++ src/components/MainContent.vue
@@ -0,0 +1,26 @@
+<script >
+
+  
+  export default {
+    components: {
+ 
+
+    },
+    data() {
+       return {
+
+       };
+    },
+  }
+</script>
+
+<template>
+ 
+  
+</template>
+
+<style lang="scss" scoped>
+
+
+
+</style>
