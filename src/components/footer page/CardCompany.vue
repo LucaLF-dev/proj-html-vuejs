8016d6395c7b9941339a1396ad2050928cb98c81
LucaLF-dev <laficaraluca@gmail.com>
2023-11-17 13:50:53 +0100

    Add - card to pagefooter

---
+++ src/components/footer page/CardCompany.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+	props: {
+		item: {
+			type: Object,
+			required: true,
+		},
+	},
+	data() {
+		return {};
+	},
+};
+</script>
+
+<template>
+	<div class="card__company">
+		
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@use "src/style/partial/_variables.scss" as *;
+.card__company{
+	
+	width: 530px;
+    
+	padding: 10px;
+}
+
+</style>
--- src/components/footer page/FooterPage.vue
+++ src/components/footer page/FooterPage.vue
@@ -1,25 +1,31 @@
 <script>
+import CardCompany from "../footer page/CardCompany.vue";
+import CardTopJackpot from "../footer page/CardTopJackpot.vue";
+
 export default {
+    components:{
+        CardCompany,CardTopJackpot,
+    },
 	data() {
 		return {
 			companyInformations: [
 				{
 					icon: "",
-					phoneNumber: "+88012345678912",
+					detail: "Phone number : +88012345678912",
 				},
 				{   icon: "", 
-                    email: "youremail@gmail.com" 
+                    detail: "Email : youremail@gmail.com" 
                 },
 				{
 					icon: "",
-					address: "30 North West New York 240",
+					detail: "Address : 30 North West New York 240",
 				},
 			],
             companyCard: [
                 {   logo:"public/logo.png",
                     description:"Upropriate brand ecomca soud techolog after covalent technology enable prospective wastng markets whereas propriate and brand economca sound technolog",
                     platforms: [
-						{ platform: "public/social-1.png"},
+						{platform:"public/social-1.png"},
 						{platform: "public/social-2.png"},
 						{platform: "public/social-3.png"},
 
@@ -43,14 +49,58 @@ export default {
                     price:"$330",
                 },
             ],
-
 		};
 	},
 };
 </script>
 
 <template>
-	<footer class="page__footer"></footer>
+	<footer class="page__footer">
+        <div class="container information__detail">
+            <div class="detail__card" v-for="information in companyInformations" :key="information "><img src="" alt=""><span>{{ information.detail }}</span></div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <CardCompany/>
+                <CardTopJackpot/>
+                <div class="card__newsletter">
+                    <div class="newsletter__header" >
+                        <h4></h4> 
+                        <p></p>
+                    </div>
+                    <div class="newsletter__body">
+                        <input type="text" placeholder=" Your Name ">
+                        <input type="text" placeholder=" Your Email">
+                        <a class="button-big white" hre="#"><span>Send message</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+.page__footer {
+    width: 100%;
+    height: 1000px;
+    background-image: url('public/bg-footer.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+
+.container {
+    &.information__detail {
+        
+        display: flex;
+        flex-direction: row;
+        
+       .detail__card {
+           padding: 20px;
+           border: 1px solid white;
+           border-top: none;
+       }
+    }
+}
+</style>
