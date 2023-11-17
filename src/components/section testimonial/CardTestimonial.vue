d910768be46ca9f5c2b49d777ffc76b049abb182
LucaLF-dev <laficaraluca@gmail.com>
2023-11-17 12:26:19 +0100

    Add - style to sectionTastimonial an cardTestimonial

--- src/components/section testimonial/CardTestimonial.vue
+++ src/components/section testimonial/CardTestimonial.vue
@@ -14,28 +14,31 @@ export default {
 
 <template>
 	<div class="card__testimonial">
-        <div class="row">
-            <div class="profile">
-                <figure>
-                    <img src="" alt="">
-                </figure>
-                <div>
-                    <h4 class="profile__name"></h4>
-                    <h6 class="profile__job"></h6>
-                </div>
-                <div class="profile__logo-team">
-                     <img src="" alt="">
-                </div>
+		<div class="row card__header">
+			<div class="profile">
+				<figure>
+					<img :src="item.imgProfile" alt="" />
+				</figure>
+				<div>
+					<h4 class="profile__name">{{ item.name }}</h4>
+					<h6 class="profile__job">{{ item.job }}</h6>
+				</div>
             </div>
-        </div>
-        <div class="row">
-            <p class="profile__description"></p>
-            <ul class="vote-star">
-                <li></li>
-            </ul>
-            <span class="vote"></span>
-        </div>
-		
+				<div class="profile__logo-team">
+					<img :src="item.imgTeam" alt="" />
+				</div>
+			
+		</div>
+		<div class="row card__body">
+			<p class="profile__description">{{ item.description }}</p>
+			<ul class="vote-star">
+				<li v-for="n in 5" class="star--centered bg--green">
+					<font-awesome-icon :icon="['fas', 'star']" />
+				</li>
+                <span class="vote">{{ item.vote }}</span>
+			</ul>
+			
+		</div>
 	</div>
 </template>
 
@@ -44,9 +47,73 @@ export default {
 .card__testimonial {
 	background-color: $card-color;
 	width: 530px;
-    
-	padding: 10px;
-}
-    
+	padding: 15px 10px;
 
+    .card__header {
+        justify-content: space-between;
+        align-items: center;
+        padding-bottom: 30px;
+        border-bottom: 1px solid white;
+        .profile {
+            display: flex;
+            align-items: center;
+            gap: 20px;
+
+            h4 {
+			text-transform: uppercase;
+		}
+		h6 {
+			font-weight: 300;
+		}
+		figure {
+			border-radius: 100%;
+			overflow: hidden;
+			width: 64px;
+			aspect-ratio: 1;
+
+			img {
+				object-fit: cover;
+				object-position: center;
+			}
+		}
+           
+        }
+
+        .profile__logo-team {
+
+            img {
+                width: 60px;
+                aspect-ratio: 1;
+            }
+        }
+    }
+	
+    .card__body {
+        padding-top: 20px;
+        display: flex;
+        flex-direction: column;
+        text-align: start;
+
+        .vote-star {
+            padding-top: 10px;
+            display: flex;
+            gap: 7px;
+        }
+    }
+}
+
+.vote-star {
+	.bg--green {
+		width: 32px;
+		aspect-ratio: 1;
+		border-radius: 5px;
+		background-color: #02b57d;
+	}
+
+	.star--centered {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+}
 </style>
--- src/components/section testimonial/SectionTestimonial.vue
+++ src/components/section testimonial/SectionTestimonial.vue
@@ -1,39 +1,66 @@
 <script>
 import CardTestimonial from "../section testimonial/CardTestimonial.vue";
 
-
 export default {
 	components: {
 		CardTestimonial,
-		
 	},
 	data() {
 		return {
-			
-		};
+            testimonialCards: [
+                {
+                    imgProfile:"public/testimonial1.jpg",
+                    name:"madley pondor",
+                    job:"UI Designer",
+                    imgTeam:"src/assets/partner logo/partner2.png",
+                    description:"Edgeing nterfaces fnergstca plagiarize teams anbudng paradms rapidiousl buildcaboration anden deassharing viaing and bleeding whereas goingi forward process and monetze",   
+                    vote:"5.28"          
+                },
+                {
+                    imgProfile:"public/testimonial2.jpg",
+                    name:"oliver beddows",
+                    job:"UI Designer",
+                    imgTeam:"src/assets/partner logo/partner4.png",
+                    description:"Edgeing nterfaces fnergstca plagiarize teams anbudng paradms rapidiousl buildcaboration anden deassharing viaing and bleeding whereas goingi forward process and monetze",   
+                    vote:"5.28"                    
+                },
+            ]
+        };
 	},
 };
 </script>
 
 <template>
 	<section class="section__testimonial">
-		dcbsbcjbsjc
-     <CardTestimonial/>
+        <div class="section__name">
+				<h4>our recent news</h4>
+				<h2 class="section-title">our most popular testimonials posts</h2>
+			</div>
+     <div class="container">
+        <div class="grid">
+			<CardTestimonial v-for="card in testimonialCards" :key="card" :item="card"/>
+		</div>
+     </div>
+		
 	</section>
 </template>
 
 <style lang="scss" scoped>
-
-
 .section__testimonial {
 	background-color: #090e38;
 	width: 100%;
-    height: 800px;
-	padding: 70px 0;
+	
+	padding: 80px 0;
+    text-align: center;
 
-	
-	
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        gap: 20px;
+    }
+
+    .container {
+        padding: 30px 0;
+    }
 }
-
-
 </style>
